cfffe1171d7f61e963fd2b498fcc5a14e861d1ca
Crystal P <pittmanc82@gmail.com>
2024-02-24 00:41:50 -0800

    added bootstrap

--- js/activities.js
+++ js/activities.js
@@ -1,5 +1,31 @@
-$(document).ready(function(){
-    $(".activities-table tbody td").click(function(){
-        $(this).toggleClass("highlight");
+//User Interaction with table cells//
+$(document).ready(function() {
+    $("td").click(function() {//user select a table data cell//
+        var content = $(this).text(); //get content of cell
+
+        if (content != "Not Available") {
+            //check if content does not contain a particular string//
+            $(this).toggleClass("tdhighlight"); //add or remove class when cell is selected//
+
+            if($(this).hasClass("tdhighlight")) { //check if selected cell has class//
+               $('#displaySelected').css("visibility","visible");//make display box visible//
+               $('#displaySelected').css("margin-top","2em");//add space above display box
+               $('#result').append("<p>"+content+"<p>");//add child element with contents of cell
+            }else {
+                //if selected cell dont have class//
+                $('#result p:contains('+content+')').remove(); //remove child element
+
+                if($('@result').has('p').lenght == false) { //check if there are any child elements within parent//
+                   $('#displaySelected').css("visibility","hidden"); //make display box hidden
+                   $('#displaySelected').css("margin-top","0"); //remove spaces above display box
+
+                }
+            }
+        }
+    });
+});
+$(document).ready(function() {
+    $("td").click(function() {
+        $(this).toggleClass("green-background");
     });
 });
